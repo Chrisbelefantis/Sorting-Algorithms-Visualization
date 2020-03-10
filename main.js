ba4f8eabf5be71fdb6284c3782253280cfe51116
Chrisbelefantis <55947209+Chrisbelefantis@users.noreply.github.com>
2020-03-10 18:30:09 +0200

    Centering the array bars

--- main.js
+++ main.js
@@ -1,37 +1,85 @@
-const crossBtn = document.querySelector(".cross");
-const modal = document.querySelector(".modal");
+// /*********************Modal Code**********************/
 
-crossBtn.addEventListener("click", () => {
-  modal.classList.add("disable");
+// const crossBtn = document.querySelector(".cross");
+// const modal = document.querySelector(".modal");
 
-  setTimeout(() => {
-    modal.style.visibility = "hidden";
-  }, 500);
-});
+// crossBtn.addEventListener("click", () => {
+//   modal.classList.add("disable");
+
+//   setTimeout(() => {
+//     modal.style.visibility = "hidden";
+//   }, 500);
+// });
+
+// /*****************************************************/
+
+
 
 const printField = document.querySelector(".visualization-field");
 const slider = document.querySelector(".slider");
+const startBtn = document.querySelector(".btn-start");
+
+function bubbleSort(numbers){
+
+  for(let i=0; i<numbers.length-1; i++){
+    for(let j=0; j<numbers.length-1; j++){
+      
+      printArray(numbers);
+      if(numbers[j]>numbers[j+1]){
+        let temp = numbers[j];
+        numbers[j] = numbers[j+1];
+        numbers[j+1] = temp;
+      }
+
+
+
+    }
+
+
+  }
+
+  console.log(numbers);
+
+}
+
 
 function printArray(numbers) {
+  
   printField.innerHTML = " ";
 
   numbers.forEach(element => {
-    let myStyle = "height:" + element + "%";
+
+    let barHeight = "height:" + element + "%";
     printField.innerHTML +=
-      '<div class="vertical-bar" style = "' + myStyle + '"></div>';
+      '<div class="vertical-bar" style = "' + barHeight + '"></div>';
+
+
+
   });
 }
 
-slider.addEventListener("change", () => {
-  let numbers = [];
+let numbers;
 
+slider.addEventListener("change", () => {
+
+  numbers = [];
   numberOfArrays = document.querySelector("#points").value;
-  console.log(numberOfArrays);
 
   for (let i = 0; i < numberOfArrays; i++) {
     randomNumber = Math.random() * 90;
     numbers.push(Math.floor(randomNumber));
   }
 
+  let visualizationFieldWidth = 8 * numberOfArrays;
+  printField.style.width ="" + visualizationFieldWidth + "px";
+
   printArray(numbers);
+});
+
+
+
+startBtn.addEventListener("click",()=>{
+
+  bubbleSort(numbers);
+
 });
